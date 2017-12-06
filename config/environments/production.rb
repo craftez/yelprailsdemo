3ea612e0115b299601cf7efad2191aeb7a5503aa
Eduardo Zamora <ezamora@trifinlabs.com>
2017-12-05 22:16:44 -0600

    add users accounts with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for devise gem remember to change to actual application host
+  config.action_mailer.default_url_options = { host: 'localhost' }
+
 end
