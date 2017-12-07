3d63bcc207404f594cab82694087d35e1ef7aa42
Eduardo Zamora <ezamora@trifinlabs.com>
2017-12-06 23:59:28 -0600

    Add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :reviews, dependent: :destroy
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: {
+    with: /\A\(\d{3}\)\s\d{3}\-\d{4}\z/, message: 'Must be in the format (123) 456-7890'
+  }
+  validates :website, format: {
+    with: /https?:\/\/.*/,
+    message: 'Must be in the format http:// or https://'
+  }
 end
